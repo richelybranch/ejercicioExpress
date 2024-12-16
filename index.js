6198e83e504536f18a2fbe0a546e57f24dea5368
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2024-12-16 10:51:42 -0500

    avance de clase

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
 app.use(express.json());
 
 var lista = [{
     id: '1',
     nombre: 'Yesenia'
-},{
+}, {
     id: '2',
     nombre: 'Yeison'
 }];
 
-//web service de tipo GET
-
-app.get('/nombre', (req, res)=>{
-    res. json(lista);
+// Web service de tipo GET
+app.get('/nombre', (req, res) => {
+    res.json(lista);
 });
 
-app.listen(3000,(){
+app.get('/richely', (req, res)=>{
+    res.send('Hola soy Richely branchito, me gusta la musica y todo lo que marea');
+});
+
+app.get('/suma', (req, resp)=>{
+    let n1=4;
+    let n2=9;
+    let suma = n1+n2;
+    resp.send(suma+'');
+});
+
+
+app.get('/suma/:n1', (req, resp) => {
+    let num1 = parseInt(req.params.n1); // Acceder al parámetro de la ruta correctamente
+    let num2 = 9;
+    let suma = num1 + num2;
+    resp.send('El resultado de la suma es: ' + suma);
+});
+
+app.listen(3000, () => {
     console.log('MELO');
-})
+});
